30c9ff3ecae0a9446624ebd606ca3c012b5b2622
abdessattarElyagoubi <126204978+abdessattarElyagoubi@users.noreply.github.com>
2023-06-07 15:58:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,73 +1,3 @@
-const express = require('express');
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-let status = {};
-
-// Middleware to parse JSON in request body
-app.use(express.json());
-
-// Function to check the availability of a website
-async function checkWebsite(website) {
-  try {
-    await axios.head(website.url);
-    status[website.name] = 'Up'; // Website is up
-    console.log(`${website.name} is up`);
-  } catch (error) {
-    status[website.name] = 'Down'; // Website is down
-    console.log(`${website.name} is down`);
-  }
-}
-
-// Function to periodically update the status of the websites
-async function updateStatus() {
-  // Read the website configurations from config.json
-  const websites = JSON.parse(fs.readFileSync('config.json'));
-
-  await Promise.all(websites.map(checkWebsite));
-
-  // Wait for 5 seconds before updating the status again
-  setTimeout(updateStatus, 5000);
-}
-
-// API endpoint to get the status of the websites
-app.get('/status', async (req, res) => {
-  // Read the website configurations from config.json
-  const websites = JSON.parse(fs.readFileSync('config.json'));
-
-  // Check the status of each website
-  const currentStatus = websites.map((website) => ({
-    name: website.name,
-    url: website.url,
-    status: status[website.name] || 'Unknown',
-  }));
-
-  res.json(currentStatus);
-});
-
-// API endpoint to add a new website
-app.post('/website', (req, res) => {
-  const { name, url } = req.body;
-
-  // Read the current websites from config.json
-  const websites = JSON.parse(fs.readFileSync('config.json'));
-
-  // Add the new website to the list
-  websites.push({ name, url });
-
-  // Write the updated list of websites to config.json
-  fs.writeFileSync('config.json', JSON.stringify(websites));
-
-  // Update the status object with the new website
-  status[name] = 'Unknown';
-
-  res.sendStatus(200);
-});
-
 // API endpoint to delete a website
 app.delete('/website/:name', (req, res) => {
   const name = req.params.name;
@@ -157,7 +87,7 @@ app.get('/admin', (req, res) => {
           deleteForm.addEventListener('submit', async (event) => {
             event.preventDefault();
             const name = deleteName.value;
-            await fetch(`/website/${name}`, {
+            await fetch('/website/' + name, {
               method: 'DELETE'
             });
             location.reload();
@@ -167,12 +97,4 @@ app.get('/admin', (req, res) => {
     </html>
   `;
   res.send(adminPageHtml);
-});
-
-// Start updating the status of the websites
-updateStatus();
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
