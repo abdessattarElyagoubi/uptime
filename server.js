c9cb1b14b305511497420bd2913cc2564cfd9bce
abdessattarElyagoubi <126204978+abdessattarElyagoubi@users.noreply.github.com>
2023-06-07 15:11:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,28 +5,48 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let status = {};
+
 // Function to check the availability of a website
 async function checkWebsite(website) {
   try {
-    await axios.get(website.url);
-    return true; // Website is up
+    await axios.head(website.url);
+    status[website.name] = 'Up'; // Website is up
+    console.log(`${website.name} is up`);
   } catch (error) {
-    return false; // Website is down
+    status[website.name] = 'Down'; // Website is down
+    console.log(`${website.name} is down`);
   }
 }
 
-app.get('/', async (req, res) => {
+// Function to periodically update the status of the websites
+async function updateStatus() {
   // Read the website configurations from config.json
   const websites = JSON.parse(fs.readFileSync('config.json'));
 
-  const results = await Promise.all(websites.map(checkWebsite));
-  const status = websites.map((website, index) => ({
+  await Promise.all(websites.map(checkWebsite));
+
+  // Wait for 5 seconds before updating the status again
+  setTimeout(updateStatus, 5000);
+}
+
+// API endpoint to get the status of the websites
+app.get('/status', async (req, res) => {
+  // Read the website configurations from config.json
+  const websites = JSON.parse(fs.readFileSync('config.json'));
+
+  // Check the status of each website
+  const currentStatus = websites.map((website) => ({
     name: website.name,
     url: website.url,
-    status: results[index] ? 'Up' : 'Down',
+    status: status[website.name] || 'Unknown',
   }));
-  res.json(status);
+
+  res.json(currentStatus);
 });
+
+// Start updating the status of the websites
+updateStatus();
 
 // Start the server
 app.listen(PORT, () => {
